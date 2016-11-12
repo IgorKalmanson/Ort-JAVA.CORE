951bd5f7356319ac8d1789af54fc3cfac69f6560
IgorKalmanson <gashlodp@gmail.com>
2016-11-12 17:30:48 +0200

    Дан массив найти сумму элементов массива значение которых < 20.

---
+++ src/Array/main.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class main {
+
+	public static void main(String[] args) {
+		int array [] = new int [20];
+		for (int i=0; i<array.length; i++){
+			array[i] = ((int)(Math.random()*20));
+			 System.out.println(array[i]);
+		
+		}
+		int summ=0;
+			for (int i = 0; i < array.length; ++i)  {
+            	if (array[i]<20) summ = summ + array[i];
+            	System.out.print(summ +" ");
+            }
+            System.out.println();
+}
+
+}
